bc8139ba77badae2f62bec6ced008e1d40c895e7
Daniel W. Robert <danielwrobert@gmail.com>
2020-06-19 23:44:45 -0400

    Add sitemap plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,12 @@ module.exports = {
 	siteMetadata: {
 		title: `dwr.io`,
 		description: `My Digital Notebook`,
+		siteUrl: `https://dwr.io`,
 	},
 	plugins: [
 		`gatsby-plugin-emotion`,
 		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sitemap`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
