1fcbf9098adb7cecfc9a73cefb68bb48dd708780
Daniel W. Robert <danielwrobert@gmail.com>
2020-06-27 23:09:48 -0400

    Add Footer component unit tests.

---
+++ src/components/__tests__/footer.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Footer from '../footer';
+
+test('Footer', () => {
+	const { container, getByText } = render(<Footer />);
+	const socialLinks = container.querySelectorAll('svg');
+	const twitterIcon = getByText(`Twitter`);
+	const githubIcon = getByText(`GitHub`);
+
+	// Renders a paragraph element (copyright)
+	expect(container.querySelector(`p`)).toBeInTheDocument();
+
+	// Renders a svg element for social links
+	expect(container.querySelector(`svg`)).toBeInTheDocument();
+
+	// Renders two svg elements for social links (Twitter and GitHub)
+	expect(socialLinks.length).toBe(2);
+
+	// Renders Twitter Icon (title)
+	expect(twitterIcon).toBeInTheDocument();
+
+	// Renders GitHub Icon (title)
+	expect(githubIcon).toBeInTheDocument();
+});
