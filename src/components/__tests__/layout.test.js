ee0c75a29633333d86337b58bb8b2bf537bc5a61
Daniel W. Robert <danielwrobert@gmail.com>
2020-06-27 22:03:02 -0400

    Update Layout test to include child render.

--- src/components/__tests__/layout.test.js
+++ src/components/__tests__/layout.test.js
@@ -4,12 +4,15 @@ import { render } from '@testing-library/react';
 import Layout from '../layout';
 
 test('Layout', () => {
-	const { container } = render(
+	const text = `__Bonjour__`;
+	const { container, getByText } = render(
 		<Layout>
-			<h1>Bonjour!</h1>
+			<h1>{text}</h1>
 		</Layout>
 	);
+	const child = getByText(text);
 
 	expect(container.querySelector(`header`)).toBeInTheDocument();
 	expect(container.querySelector(`footer`)).toBeInTheDocument();
+	expect(child).toBeInTheDocument();
 });
