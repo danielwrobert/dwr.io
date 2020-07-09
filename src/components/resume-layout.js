ccb89bf8b37e725993b1ce0396b9d91947d38c51
Daniel W. Robert <danielwrobert@gmail.com>
2020-07-08 23:43:22 -0400

    Add resume grid layout.

---
+++ src/components/resume-layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from '@emotion/styled';
+// import Header from './header';
+// import Footer from './footer';
+
+import 'styles/normalize.css';
+import 'styles/global.css';
+
+const Content = styled.div`
+	margin: 0 auto;
+	max-width: 96%;
+	padding: 7rem 4rem;
+	width: 100%;
+`;
+
+export default ({ children }) => (
+	<>
+		{/* <Header /> */}
+		<Content>{children}</Content>
+		{/* <Footer /> */}
+	</>
+);
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,28 +1,100 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import Layout from 'components/layout';
+import ResumeLayout from 'components/resume-layout';
 import SEO from 'components/seo';
-// import Stitch from 'components/stitch';
+
+import headshot from 'images/dan-headshot.png';
 
 const Container = styled.article`
 	display: grid;
-	grid-template-columns: 2fr 1fr;
-	grid-template-rows: 1fr 1fr 1fr 1fr;
+	grid-gap: 0.5rem;
+	grid-template-columns: 1fr;
+	grid-template-rows: repeat(7, 1fr);
+	grid-template-areas:
+		'photo'
+		'name'
+		'about'
+		'work'
+		'education'
+		'skills'
+		'community';
+	/* height: calc(100vh - (14rem + 16.4rem)); */
+	height: calc(100vh - 14rem);
+
+	@media (min-width: 900px) {
+		grid-template-columns: 2fr 1fr;
+		grid-template-rows: repeat(6, 1fr);
+		grid-template-areas:
+			'name photo'
+			'about about'
+			'work work'
+			'education education'
+			'skills skills'
+			'community community';
+	}
+	@media (min-width: 1200px) {
+		grid-template-rows: repeat(4, 1fr);
+		grid-template-areas:
+			'name photo'
+			'work about'
+			'work education'
+			'community skills';
+	}
+`;
+
+const Section = styled.section`
+	border: 3px solid var(--shadow-color);
+
+	&.name {
+		border: none;
+		grid-area: name;
+	}
+	&.photo {
+		border: none;
+		grid-area: photo;
+		/* align-self: center; */
+		justify-self: center;
+
+		img {
+			border-radius: 15rem;
+			max-width: 25rem;
+		}
+	}
+	&.about {
+		grid-area: about;
+	}
+	&.work {
+		grid-area: work;
+	}
+	&.education {
+		grid-area: education;
+	}
+	&.community {
+		grid-area: community;
+	}
+	&.skills {
+		grid-area: skills;
+	}
 `;
 
 const Resume = () => (
-	<Layout>
+	<ResumeLayout>
 		<SEO title="Resume" />
 		<Container className="resume">
-			<section className="name">Name</section>
-			<section className="photo">Photo</section>
-			<section className="about">About</section>
-			<section className="work">Work</section>
-			<section className="education">Education</section>
-			<section className="community">Community</section>
-			<section className="skills">Skills</section>
+			<Section className="name">
+				<h1>Dan Robert</h1>
+				<h2>Front-End Engineer</h2>
+			</Section>
+			<Section className="photo">
+				<img src={headshot} alt="Headshot" />
+			</Section>
+			<Section className="about">About</Section>
+			<Section className="work">Work</Section>
+			<Section className="education">Education</Section>
+			<Section className="community">Community</Section>
+			<Section className="skills">Skills</Section>
 		</Container>
-	</Layout>
+	</ResumeLayout>
 );
 
 export default Resume;
