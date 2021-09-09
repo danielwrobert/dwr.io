86b9a3bd3a298210bb13542c45bde23b8a65c0e6
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2021-09-09 11:15:12 -0400

    Add and configure RSS feed via gatsby-plugin-feed-mdx. (#42)

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,30 +30,3 @@ exports.createPages = async ({ graphql, actions: { createPage }, reporter }) => 
 		});
 	});
 };
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-	const { createNodeField } = actions;
-
-	if (node.internal.type === `allMdx`) {
-		const fileNode = getNode(node.parent);
-		let nodeSlug;
-		nodeSlug = ensureSlashes(
-			path.basename(fileNode.relativePath, path.extname(fileNode.relativePath))
-		);
-		if (nodeSlug) {
-			createNodeField({ node, name: `slug`, value: nodeSlug });
-		}
-	}
-};
-
-function ensureSlashes(slug) {
-	if (slug.charAt(0) !== `/`) {
-		slug = `/` + slug;
-	}
-
-	if (slug.charAt(slug.length - 1) !== `/`) {
-		slug = slug + `/`;
-	}
-
-	return slug;
-}
