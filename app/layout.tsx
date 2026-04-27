721826f97f440a7ea363a2ff22a66307a0e1fba6
Daniel W. Robert <danielwrobert@gmail.com>
2026-04-27 09:48:44 -0400

    Refactor layout styles: add background and text color classes to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
       lang="en"
       className={`${ovo.variable} ${mulish.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background text-text font-sans leading-relaxed">
+        {children}
+      </body>
     </html>
   );
 }
