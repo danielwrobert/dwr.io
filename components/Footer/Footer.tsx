793cd19db71acb36fcaba639fa850f01c6ca92c0
Daniel W. Robert <danielwrobert@gmail.com>
2026-05-11 14:58:59 -0400

    Remove the term 'copyright' in favor of just the icon.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import SocialLinks from '@/components/SocialLinks/SocialLinks';
 export default function Footer() {
   return (
     <footer className="bg-shadow mt-auto p-6 sm:flex sm:items-center sm:justify-between">
-      <p className="m-0 mr-5">Copyright &copy; {new Date().getFullYear()} DWR.IO</p>
+      <p className="m-0 mr-5">&copy; {new Date().getFullYear()} DWR.IO</p>
       <SocialLinks />
     </footer>
   );
