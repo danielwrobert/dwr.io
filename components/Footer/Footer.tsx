c9072f3015cac4b419b61ff3f4b8a909309256f7
Daniel W. Robert <danielwrobert@gmail.com>
2026-05-21 08:26:06 -0400

    Update footer layout to center content on mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import SocialLinks from '@/components/SocialLinks/SocialLinks';
 
 export default function Footer() {
   return (
-    <footer className="bg-shadow mt-auto p-6 sm:flex sm:items-center sm:justify-between">
+    <footer className="bg-shadow mt-auto p-6 flex flex-col items-center sm:flex-row sm:items-center sm:justify-between">
       <p className="m-0 mr-5">&copy; {new Date().getFullYear()} DWR.IO</p>
       <SocialLinks />
     </footer>
