dd3bc91522252cb65558cd39f7b9421671aea740
Daniel W. Robert <danielwrobert@gmail.com>
2026-06-28 16:24:13 -0400

    Commit for removed/renamed files (false positive?)

--- components/TOC/TOC.tsx
+++
@@ -1,8 +0,0 @@
-export default function TOC({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-shadow rounded-sm p-4 my-[1.125rem]">
-      <h2 className="!text-highlight-4">Series Table of Contents:</h2>
-      {children}
-    </div>
-  );
-}
